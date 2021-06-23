22bb05a26492bf567bd585014a468aa851d7218a
Jithin D K <43891353+jithindk@users.noreply.github.com>
2021-06-23 18:19:37 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,39 @@
+import requests
+from datetime import datetime
+
+#Use your own api key
+#I have deleted my api key
+user_api = "2417f992f93944951cb25f8e172000b9"
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+#create variables to store and display data
+
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+#to save as a text file
+with open('weather.txt','a') as f:
+    f.write("-------------------------------------------------------------\n")
+    f.write("Weather Stats for - {}  || {}\n".format(location.upper(), date_time))
+    f.write("-------------------------------------------------------------\n")
+    f.write("Current temperature is: {:.2f} deg C\n".format(temp_city))
+    f.write("Current weather desc  : {}\n".format(weather_desc))
+    f.write("Current Humidity      : {} %\n".format(hmdt))
+    f.write("Current wind speed    : {} kmph\n".format(wind_spd))
+    f.write("\n\n")
+
+#to print the data
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
